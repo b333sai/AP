f24d25027742e3e83de568adff6d92a591fb0d7f
sai kumar <battinojusaikumar@gmail.com>
2013-05-10 18:21:17 +0530

    v1
    Its the starting stage of the project

---
+++ home.php
@@ -0,0 +1,13 @@
+<?php
+ echo '</br>
+ <center><h1>Welcome to the admission portal - 2013</h1>
+ </br></br>
+ <h2>Select you stream</h2>
+ </br>
+ <table id="table1" >
+ <tr><td id="select" ></td><td><a href="engineering.php" >Engineering</a></td></tr>
+ <tr><td id="select" ></td><td><a href="medical.php" >Medical</a></td></tr>
+ <tr><td id="select" ></td><td><a href="management.php" >Management</a></td></tr>
+ </table></center></br></br></br></br></br></br></br></br></br></br>
+ ';
+?>
---
+++ management.php
@@ -0,0 +1,14 @@
+<?php
+?>
+
+<?php
+ include("header.php");
+?>
+<?php
+ echo '<div id=page_container>
+       <div id=content>
+	   Hai</div></div>';
+?>
+<?php
+ include("footer.php");
+?>
